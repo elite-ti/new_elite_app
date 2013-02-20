68958693ef60b20261634d80f71611694106bd24
Gustavo Schmidt <gustavo.schmidt@sistemaeliterio.com.br>
2013-02-20 03:55:20 -0300

    Fix deploy task

--- config/recipes/card_scanner.rb
+++ config/recipes/card_scanner.rb
@@ -1,7 +1,7 @@
 namespace :card_scanner do
   desc "Compile card scanner"
   task :compile, roles: :app do
-    run "ruby #{release_path}/lib/card_scanner/type_b/compile.rb"
+    run "ruby #{current_path}/lib/card_scanner/type_b/compile.rb"
   end
   after "deploy:finalize_update", "card_scanner:compile"
 end
