bbc9946abb78b7ba1056477fd1e39a514f6bd078
Gustavo Schmidt <gustavo.schmidt@sistemaeliterio.com.br>
2013-02-20 20:12:42 -0300

    Fix employee bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,21 +20,21 @@ protected
 private
 
   def current_ability
-    @current_ability ||= set_ability
+    @current_ability ||= set_current_ability
   end
   helper_method :current_ability
 
-  def set_ability
+  def set_current_ability
     return GuestAbility.new if current_role.nil?
     return "#{current_role.camelize}Ability".constantize.new(current_employee)
   end
 
   def current_role
-    @current_role ||= set_role
+    @current_role ||= set_current_role
   end
   helper_method :current_role
 
-  def set_role
+  def set_current_role
     return nil if current_employee.nil?
 
     if current_employee.roles.include? session[:role]
@@ -45,11 +45,11 @@ private
   end
 
   def current_employee
-    @current_employee ||= set_employee
+    @current_employee ||= set_current_employee
   end
   helper_method :current_employee
 
-  def set_employee
+  def set_current_employee
     Employee.find(session[:user_id]) if session[:user_id]
   end
 end
