f6a59f0b9ee7e89b9a92d589e70841f100d449e1
Gustavo Schmidt <gustavoschmidt11@gmail.com>
2013-03-18 20:55:58 -0300

    Update exam execution name method

--- app/models/exam_execution.rb
+++ app/models/exam_execution.rb
@@ -11,6 +11,6 @@ class ExamExecution < ActiveRecord::Base
   validates :exam_cycle, :super_klazz, :exam, presence: :true 
 
   def name
-    exam_cycle.name + ' - ' + super_klazz.name
+    exam_cycle.name + ' - ' + super_klazz.name + ' - ' + exam.name
   end
 end 
