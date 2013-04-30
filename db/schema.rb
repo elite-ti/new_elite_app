266cdb679805495f1872e8ff79f8e2d9b0267c12
Paulo André <pauloacmelo@gmail.com>
2013-04-30 18:35:32 -0300

    Fixed remove_index syntax

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,18 @@
-ActiveRecord::Schema.define(:version => 20130217201533) do
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20130426063009) do
+
   create_table "absence_reasons", :force => true do |t|
     t.string   "name"
     t.datetime "created_at"
@@ -25,6 +39,17 @@ ActiveRecord::Schema.define(:version => 20130217201533) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "answer_card_types", :force => true do |t|
+    t.string   "name",                  :null => false
+    t.string   "parameters",            :null => false
+    t.string   "card",                  :null => false
+    t.integer  "student_number_length", :null => false
+    t.datetime "created_at",            :null => false
+    t.datetime "updated_at",            :null => false
+  end
+
+  add_index "answer_card_types", ["name"], :name => "index_answer_card_types_on_name", :unique => true
 
   create_table "applicants", :force => true do |t|
     t.integer  "number"
@@ -166,6 +191,9 @@ ActiveRecord::Schema.define(:version => 20130217201533) do
     t.datetime "updated_at"
   end
 
+  add_index "exam_answers", ["exam_question_id"], :name => "index_exam_answers_on_exam_question_id"
+  add_index "exam_answers", ["student_exam_id"], :name => "index_exam_answers_on_student_exam_id"
+
   create_table "exam_cycles", :force => true do |t|
     t.string   "name"
     t.boolean  "is_bolsao"
@@ -189,6 +217,13 @@ ActiveRecord::Schema.define(:version => 20130217201533) do
     t.integer  "question_id"
     t.integer  "exam_id"
     t.integer  "number"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "exam_subjects", :force => true do |t|
+    t.integer  "subject_id"
+    t.integer  "exam_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -562,6 +597,14 @@ ActiveRecord::Schema.define(:version => 20130217201533) do
     t.datetime "created_at"
   end
 
+  create_table "year_products", :force => true do |t|
+    t.string   "name"
+    t.string   "year_id"
+    t.integer  "product_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "years", :force => true do |t|
     t.integer  "number"
     t.date     "start_date"
@@ -569,4 +612,5 @@ ActiveRecord::Schema.define(:version => 20130217201533) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
 end
