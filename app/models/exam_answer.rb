87e566549fcd86f716f9d1a1310f7fe2ec8dd060
Paulo André <pauloacmelo@gmail.com>
2013-10-14 12:03:18 -0300

    Added Question Number to InvalidAnswers solving

--- app/models/exam_answer.rb
+++ app/models/exam_answer.rb
@@ -3,6 +3,8 @@ class ExamAnswer < ActiveRecord::Base
   ANSWERS_TO_BE_CHECKED = %w[W Z]
 
   attr_accessible :answer, :exam_question_id, :student_exam_id
+  delegate :number, to: :exam_question
+
   belongs_to :exam_question
   belongs_to :student_exam
 
