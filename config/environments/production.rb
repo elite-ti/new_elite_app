26a2850e0fc9f2e2798a1e9ca1a3b61c9ab1e24e
Paulo André <pauloacmelo@gmail.com>
2013-11-09 03:31:48 -0200

    Added email confirmation to card_processing#create

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,22 @@ EliteApp::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # ActionMailer configurations
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.default_url_options = { :host => "elitesim.sistemaeliterio.com.br" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default charset: 'utf-8'
+  config.action_mailer.smtp_settings = {
+    authentication: :plain,
+    address: 'smtp.gmail.com',
+    port: '587',
+    domain: 'gmail.com',
+    user_name: 'elitesim@sistemaeliterio.com.br',
+    password: 'Er21CkOy',
+    enable_starttls_auto: true
+  }
+
 end
