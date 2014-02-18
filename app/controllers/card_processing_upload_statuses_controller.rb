c31d25028c4a742e62360ff1a288e622a3b5e769
Paulo André <pauloacmelo@gmail.com>
2014-02-18 15:09:56 -0300

    Fixed csv download

--- app/controllers/card_processing_upload_statuses_controller.rb
+++ app/controllers/card_processing_upload_statuses_controller.rb
@@ -16,15 +16,21 @@ class CardProcessingUploadStatusesController < ApplicationController
   def show
     campus_ids = Campus.accessible_by(current_ability).map(&:id)
     exam_date = params[:id].to_date
-    @results = 
+    base =  
       StudentExam.where(
         status: StudentExam::VALID_STATUS,
         exam_execution_id:
           ExamExecution.where(
-            datetime: (exam_date.beginning_of_day)..(exam_date.end_of_day), 
-            super_klazz_id: SuperKlazz.where(campus_id: campus_ids).map(&:id)
-          ).map(&:id)
-      ).map{|student_exam| ["1", "301", "N", "%06d" % (student_exam.student.ra || 0),student_exam.grades].join(';')}
+            datetime: (exam_date.beginning_of_day)..(exam_date.end_of_day)
+          ).includes(:exam_cycle).select{|ee| !ee.is_bolsao}.map(&:id)
+      )
+    @results =
+      base.select{|student_exam| student_exam.student.ra.to_s.size <= 6}.map do |student_exam|
+        student_exam.grades.split(',').each_slice(student_exam.grades.split(',').size/2).map do |array|
+          (["1", "301", "N", "%06d" % (student_exam.student.ra || 0)] + array)
+        end
+      end.flatten(1)
+
     respond_to do |format|
       format.html
       format.csv { render text: @results.to_csv }
