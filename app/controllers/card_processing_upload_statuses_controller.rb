e3ea5dba9187699e50332ff988fbe7f64123e82c
Paulo André <pauloacmelo@gmail.com>
2014-02-25 22:38:50 -0300

    Fixed day_result#result calculation

--- app/controllers/card_processing_upload_statuses_controller.rb
+++ app/controllers/card_processing_upload_statuses_controller.rb
@@ -23,11 +23,11 @@ class CardProcessingUploadStatusesController < ApplicationController
           ExamExecution.where(
             datetime: (exam_date.beginning_of_day)..(exam_date.end_of_day)
           ).includes(:exam_cycle).select{|ee| !ee.is_bolsao}.map(&:id)
-      )
+      ).includes(:student)
     @results =
       base.select{|student_exam| student_exam.student.ra.to_s.size <= 6}.map do |student_exam|
         student_exam.grades.split(',').each_slice(student_exam.grades.split(',').size/2).map do |array|
-          (["1", "301", "N", "%06d" % (student_exam.student.ra || 0)] + array).join(';')
+          (["1", "302", "N", "%06d" % (student_exam.student.ra || 0)] + array).join(';')
         end
       end.flatten.join("\r\n")
 
