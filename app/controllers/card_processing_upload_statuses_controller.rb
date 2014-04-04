57875ce5e851c28482415645cd4fa22e413b8e27
Paulo André <pauloacmelo@gmail.com>
2014-04-04 14:48:51 -0300

    Fixed download of markings and card_data for exam_day

--- app/controllers/card_processing_upload_statuses_controller.rb
+++ app/controllers/card_processing_upload_statuses_controller.rb
@@ -8,12 +8,12 @@ class CardProcessingUploadStatusesController < ApplicationController
   #   @possible_dates = ExamExecution.all.map(&:datetime).map(&:to_date).uniq.sort!
 
   # end
-  
+
   def index
     exam_executions = ExamExecution.where(exam_cycle_id: ExamCycle.where(is_bolsao: false))
     @possible_dates = exam_executions.map(&:datetime).map(&:to_date).uniq.sort
     @amounts = Hash[* @possible_dates.map{|date| [date,0]}.flatten + [Date.new(1900, 1, 1), 0]]
-    
+
     translations = Hash[* exam_executions.map{|exam_execution| [exam_execution.id, exam_execution.datetime.to_date]}.flatten]
     translations[nil] = Date.new(1900, 1, 1)
     StudentExam.where(status: 'Valid').count(group: "exam_execution_id").each do |exam_execution_id, count|
@@ -24,7 +24,7 @@ class CardProcessingUploadStatusesController < ApplicationController
 
   def results
     exam_date = params[:id].to_date
-    base =  
+    base =
       StudentExam.where("grades is not null").where(
         status: StudentExam::VALID_STATUS,
         exam_execution_id:
@@ -32,7 +32,7 @@ class CardProcessingUploadStatusesController < ApplicationController
             datetime: (exam_date.beginning_of_day)..(exam_date.end_of_day)
           ).includes(:exam_cycle).select{|ee| !ee.is_bolsao}.map(&:id)
       ).includes(
-        student: {enrollments: :super_klazz}, 
+        student: {enrollments: :super_klazz},
         exam_execution: [:exam, { super_klazz: [:campus, product_year: :product]}]
       )
     @results =
@@ -49,9 +49,9 @@ class CardProcessingUploadStatusesController < ApplicationController
     end
   end
 
-  def markings
+  def scanned
     exam_date = params[:id].to_date
-    @results =  
+    @results = (['*VALORES POSSIVELMENTE ALTERADOS PELOS COORDENADORES', 'RA ALUNO;CODIGO PROVA;RESPOSTAS'] +
       StudentExam.where("exam_answer_as_string is not null").where(
         status: StudentExam::VALID_STATUS,
         exam_execution_id:
@@ -61,7 +61,13 @@ class CardProcessingUploadStatusesController < ApplicationController
           )
       ).includes(
         :student
-      ).map{|student_exam| ("%06d" % (student_exam.student.try(:ra) || 0)) + student_exam.exam_answer_as_string}.compact.join("\r\n")
+      ).map do |student_exam|
+        [
+          ("%06d" % (student_exam.student.try(:ra) || 0)),
+          ("%05d" % (student_exam.exam_execution.try(:exam).try(:code) || 0)),
+          student_exam.exam_answer_as_string
+        ].join(';')
+      end.compact).join("\r\n")
 
     respond_to do |format|
       format.html
@@ -69,18 +75,20 @@ class CardProcessingUploadStatusesController < ApplicationController
     end
   end
 
-  def scanned
+  def markings
     exam_date = params[:id].to_date
-    @results = 
+    @results = (['ID CARTAO;RA ALUNO;CODIGO PROVA;RESPOSTAS'] +
       StudentExam.where(
         card_processing_id: CardProcessing.where(is_bolsao: false, exam_date: exam_date).map(&:id)
-      ).map do |student_exam| 
+      ).map do |student_exam|
         student_exam.student_number = (student_exam.student_number.to_i / 10).to_s if student_exam.student_number.size > 6
         [
-          student_exam.id, 
-          'Z' * (6 - student_exam.student_number.size) + student_exam.student_number + student_exam.string_of_answers + 'Z' * (100 - student_exam.string_of_answers.size)
+          student_exam.id,
+          'Z' * (6 - student_exam.student_number.size) + student_exam.student_number,
+          'Z' * (5 - student_exam.exam_code.size) + student_exam.exam_code,
+          student_exam.string_of_answers + 'Z' * (100 - student_exam.string_of_answers.size)
         ].join(';')
-      end.compact.join("\r\n")
+      end.compact).join("\r\n")
 
     respond_to do |format|
       format.html
@@ -106,7 +114,7 @@ class CardProcessingUploadStatusesController < ApplicationController
         se.update_column(:grades, grades.to_a.flatten.join(','))
       end
       break
-    end  
+    end
     redirect_to card_processing_upload_statuses_url, notice: "Notas recalculadas com sucesso."
   end
 
