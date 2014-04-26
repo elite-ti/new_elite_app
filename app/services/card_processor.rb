18cb0309caf19da6a2897e8696a4a42f7b34e371
Paulo André <pauloacmelo@gmail.com>
2014-04-26 00:53:08 -0300

    MEMORY LEAK - Try #1: create_student_exams

--- app/services/card_processor.rb
+++ app/services/card_processor.rb
@@ -11,28 +11,28 @@ class CardProcessor
   def process
     begin
       create_student_exams
-      scan
-      ActionMailer::Base.mail(
-        from: 'elitesim@sistemaeliterio.com.br',
-        to: @card_processing.employee.try(:email) || 'elitesim@sistemaeliterio.com.br',
-        subject: "Envio arquivo ##{@card_processing.id}",
-        body: <<-eos
-        Olá,
+      # scan
+      # ActionMailer::Base.mail(
+      #   from: 'elitesim@sistemaeliterio.com.br',
+      #   to: @card_processing.employee.try(:email) || 'elitesim@sistemaeliterio.com.br',
+      #   subject: "Envio arquivo ##{@card_processing.id}",
+      #   body: <<-eos
+      #   Olá,
 
-        Você acaba de enviar um arquivo para o EliteSim.
+      #   Você acaba de enviar um arquivo para o EliteSim.
 
-        Prova: #{@card_processing.name}
-        Data prova: #{@card_processing.exam_date.strftime('%d/%m/%Y')}
-        Data Envio: #{@card_processing.created_at.strftime('%d/%m/%Y %H:%M')}
-        Unidade: #{@card_processing.campus.name}
-        Tipo de Cartão: #{@card_processing.card_type.name}
-        Quantidade de cartões: #{@card_processing.student_exams.size}
-        Quantidade de erros: #{@card_processing.number_of_errors}
+      #   Prova: #{@card_processing.name}
+      #   Data prova: #{@card_processing.exam_date.strftime('%d/%m/%Y')}
+      #   Data Envio: #{@card_processing.created_at.strftime('%d/%m/%Y %H:%M')}
+      #   Unidade: #{@card_processing.campus.name}
+      #   Tipo de Cartão: #{@card_processing.card_type.name}
+      #   Quantidade de cartões: #{@card_processing.student_exams.size}
+      #   Quantidade de erros: #{@card_processing.number_of_errors}
 
-        --
-        Central de Resultados
-        eos
-      ).deliver
+      #   --
+      #   Central de Resultados
+      #   eos
+      # ).deliver
     rescue Exception => e
       p 'ERRO! ' + e.message 
       p e.backtrace.inspect          
