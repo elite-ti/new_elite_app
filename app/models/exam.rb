f78cf3b2c3a40af85cd41bb23168fb2d47de0ab7
Paulo André <pauloacmelo@gmail.com>
2014-04-28 14:12:14 -0300

    Added failsafe for correct_answers or subjects nil on exam validation

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -73,6 +73,7 @@ class Exam < ActiveRecord::Base
   end
 
   def create_questions
+    return if correct_answers.nil? || subjects.nil?
     number_of_questions = self.subjects.gsub(')', '').split('+').map{|s| s.split('(')[1].to_i}.reduce(:+)
     (1..number_of_questions).each do |question_number|
       question = Question.create!(stem: 'Stem', model_answer: 'Model Answer')
