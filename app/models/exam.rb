cfabb7e0213419385d3a3a08d97c1861df490074
Paulo André <pauloacmelo@gmail.com>
2014-04-30 14:19:41 -0300

    git added validation for exam.code uniqueness and presence

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -13,8 +13,8 @@ class Exam < ActiveRecord::Base
 
   validates :name, :options_per_question, presence: true
   validate :correct_answers_range, on: :create
+  validates :code, presence: true, uniqueness: true
 
-  
   after_create :create_questions
   before_save :update_questions, :update_subjects
 
@@ -87,6 +87,73 @@ class Exam < ActiveRecord::Base
       ExamQuestion.create!(exam_id: self.id, question_id: question.id)
     end
     update_subjects
+  end
+
+  def self.new_code
+    {
+      "2014-04-24,Manhã,6º Ano,MAT(10)"=>16019,
+      "2014-04-24,Tarde,6º Ano,MAT(10)"=>16020,
+      "2014-04-25,Manhã,6º Ano,POR(10)"=>16021,
+      "2014-04-25,Tarde,6º Ano,POR(10)"=>16022,
+      "2014-04-28,Manhã,6º Ano,CIE(10)"=>16023,
+      "2014-04-28,Tarde,6º Ano,CIE(10)"=>16024,
+      "2014-04-29,Manhã,6º Ano,GEO(10)"=>16025,
+      "2014-04-29,Tarde,6º Ano,GEO(10)"=>16026,
+      "2014-04-30,Manhã,6º Ano,HIS(10)"=>16027,
+      "2014-04-30,Tarde,6º Ano,HIS(10)"=>16028,
+      "2014-04-11,Manhã,6º Ano,ESP(10)"=>16029,
+      "2014-04-11,Tarde,6º Ano,ESP(10)"=>16030,
+      "2014-04-11,Manhã,6º Ano,ING(10)"=>16031,
+      "2014-04-11,Tarde,6º Ano,ING(10)"=>16032,
+      "2014-04-24,Manhã,7º Ano,MAT(10)"=>17019,
+      "2014-04-24,Tarde,7º Ano,MAT(10)"=>17020,
+      "2014-04-25,Manhã,7º Ano,POR(10)"=>17021,
+      "2014-04-25,Tarde,7º Ano,POR(10)"=>17022,
+      "2014-04-28,Manhã,7º Ano,CIE(10)"=>17023,
+      "2014-04-28,Tarde,7º Ano,CIE(10)"=>17024,
+      "2014-04-29,Manhã,7º Ano,GEO(10)"=>17025,
+      "2014-04-29,Tarde,7º Ano,GEO(10)"=>17026,
+      "2014-04-30,Manhã,7º Ano,HIS(10)"=>17027,
+      "2014-04-30,Tarde,7º Ano,HIS(10)"=>17028,
+      "2014-04-11,Manhã,7º Ano,ESP(10)"=>17029,
+      "2014-04-11,Tarde,7º Ano,ESP(10)"=>17030,
+      "2014-04-11,Manhã,7º Ano,ING(10)"=>17031,
+      "2014-04-11,Tarde,7º Ano,ING(10)"=>17032,
+      "2014-04-24,Manhã,8º Ano,MAT(10)"=>18019,
+      "2014-04-24,Tarde,8º Ano,MAT(10)"=>18020,
+      "2014-04-25,Manhã,8º Ano,POR(10)"=>18021,
+      "2014-04-25,Tarde,8º Ano,POR(10)"=>18022,
+      "2014-04-28,Manhã,8º Ano,CIE(10)"=>18023,
+      "2014-04-28,Tarde,8º Ano,CIE(10)"=>18024,
+      "2014-04-29,Manhã,8º Ano,GEO(10)"=>18025,
+      "2014-04-29,Tarde,8º Ano,GEO(10)"=>18026,
+      "2014-04-30,Manhã,8º Ano,HIS(10)"=>18027,
+      "2014-04-30,Tarde,8º Ano,HIS(10)"=>18028,
+      "2014-04-11,Manhã,8º Ano,ESP(10)"=>18029,
+      "2014-04-11,Tarde,8º Ano,ESP(10)"=>18030,
+      "2014-04-11,Manhã,8º Ano,ING(10)"=>18031,
+      "2014-04-11,Tarde,8º Ano,ING(10)"=>18032,
+      "2014-04-24,Manhã,1ª Série ENEM,BIO(10)+QUI(10)+FIS(10)"=>21021,
+      "2014-04-24,Tarde,1ª Série ENEM,BIO(10)+QUI(10)+FIS(10)"=>21022,
+      "2014-04-25,Manhã,1ª Série ENEM,HIS(10)+GEO(10)+FIL(10)+SOC(10)"=>21023,
+      "2014-04-25,Tarde,1ª Série ENEM,HIS(10)+GEO(10)+FIL(10)+SOC(10)"=>21024,
+      "2014-04-28,Manhã,1ª Série ENEM,POR(10)+LIT(10)+RED(10)"=>21025,
+      "2014-04-28,Tarde,1ª Série ENEM,POR(10)+LIT(10)+RED(10)"=>21026,
+      "2014-04-29,Manhã,1ª Série ENEM,ING(10)+ESP(10)"=>21027,
+      "2014-04-29,Tarde,1ª Série ENEM,ING(10)+ESP(10)"=>21028,
+      "2014-04-29,Manhã,1ª Série ENEM,MAT(10)"=>21029,
+      "2014-04-29,Tarde,1ª Série ENEM,MAT(10)"=>21030,
+      "2014-04-24,Manhã,2ª Série ENEM,BIO(10)+QUI(10)+FIS(10)"=>22021,
+      "2014-04-24,Tarde,2ª Série ENEM,BIO(10)+QUI(10)+FIS(10)"=>22022,
+      "2014-04-25,Manhã,2ª Série ENEM,HIS(10)+GEO(10)+FIL(10)+SOC(10)"=>22023,
+      "2014-04-25,Tarde,2ª Série ENEM,HIS(10)+GEO(10)+FIL(10)+SOC(10)"=>22024,
+      "2014-04-28,Manhã,2ª Série ENEM,POR(10)+LIT(10)+RED(10)"=>22025,
+      "2014-04-28,Tarde,2ª Série ENEM,POR(10)+LIT(10)+RED(10)"=>22026,
+      "2014-04-29,Manhã,2ª Série ENEM,ING(10)+ESP(10)"=>22027,
+      "2014-04-29,Tarde,2ª Série ENEM,ING(10)+ESP(10)"=>22028,
+      "2014-04-29,Manhã,2ª Série ENEM,MAT(10)"=>22029,
+      "2014-04-29,Tarde,2ª Série ENEM,MAT(10)"=>22030
+    }    
   end
 
 private
