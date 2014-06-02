14ac38e12b699c7234128abbb4e7c09247cf354b
Paulo André <pauloacmelo@gmail.com>
2014-06-02 03:13:54 -0300

    Added rubygems require

--- app/controllers/exam_executions_controller.rb
+++ app/controllers/exam_executions_controller.rb
@@ -1,5 +1,6 @@
 #encoding: utf-8
 
+require "rubygems"
 require "axlsx"
 class ExamExecutionsController < ApplicationController
   # load_and_authorize_resource
--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -1,5 +1,6 @@
 # encoding: utf-8
 
+require "rubygems"
 require "axlsx"
 class ExamsController < ApplicationController
   load_and_authorize_resource
