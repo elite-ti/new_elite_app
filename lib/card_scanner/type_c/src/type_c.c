672641ed8d629fdc8a003b33e2492dfc7b9e74d7
Paulo André <pauloacmelo@gmail.com>
2014-07-12 10:42:51 -0300

    Removed debug flag

--- lib/card_scanner/type_c/src/type_c.c
+++ lib/card_scanner/type_c/src/type_c.c
@@ -6,7 +6,7 @@
 #include <string.h>
 
 #define ERROR 0.7
-#define DEBUG // Comment out if not debug version
+// #define DEBUG // Comment out if not debug version
 // #define MAP_PERCENTAGE
 
 #define WRONG_NUMBER_OF_ARGUMENTS "Error: wrong number of arguments."
