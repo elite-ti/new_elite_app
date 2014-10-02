fd8f30969d4ae74e0c2c10c27b8add9bda36ee84
Paulo André <pauloacmelo@gmail.com>
2014-10-02 01:33:40 -0300

    Added api/cards endpoint

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,26 @@
+#encoding: utf-8
+
+class ApiController < ApplicationController
+  def index
+  end
+
+  def student_exams
+    edited_query = '%' + params[:q].gsub(' ', '%') + '%'
+    if params[:q] != '' && params[:token]='GRZezNtTYitYIXPo3seb6wsC'
+      @student_exams = CardProcessing.where(status: 'Processed').where('file like ?', edited_query).map(
+        &:student_exams
+      ).flatten.map do |student_exam|
+        [
+          student_exam.student_number,
+          student_exam.exam_code,
+          student_exam.string_of_answers
+        ]
+      end
+    else
+      @student_exams = 'Invalid token'
+    end
+    respond_to do |format|
+      format.json { render json: @student_exams}
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -97,6 +97,7 @@ EliteApp::Application.routes.draw do
   match 'login_as/:id', to: 'employee_sessions#update', as: 'login_as'
   match 'logout', to: 'employee_sessions#destroy', as: 'logout'
 
+  get 'api/cards', to:'api#student_exams', as: 'api'
   get 'home', to: 'home#index', as: 'home'
   get "exam_results", to: 'exam_results#index'  
   root to: 'home#index'
