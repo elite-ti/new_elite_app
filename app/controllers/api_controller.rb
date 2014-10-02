bd2f715347a7617b552d3065cd2633f162bf940e
Paulo André <pauloacmelo@gmail.com>
2014-10-02 02:05:58 -0300

    API Hot fix #2

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -6,7 +6,9 @@ class ApiController < ApplicationController
 
   def student_exams
     edited_query = '%' + params[:q].gsub(' ', '%') + '%'
-    if params[:q] != '' && params[:token] == 'GRZezNtTYitYIXPo3seb6wsC'
+    if params[:token] != 'GRZezNtTYitYIXPo3seb6wsC'
+      @student_exams = 'Invalid token'
+    elsif params[:q] != ''
       @student_exams = CardProcessing.where(status: 'Processed').where('file like ?', edited_query).map(
         &:student_exams
       ).flatten.map do |student_exam|
@@ -17,7 +19,7 @@ class ApiController < ApplicationController
         ]
       end
     else
-      @student_exams = 'Invalid token'
+      @student_exams = []
     end
     respond_to do |format|
       format.json { render json: @student_exams}
